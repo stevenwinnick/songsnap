b0e471a77e8d866a1e540966c5dec8e3e140fea8
Steven Winnick <winnicksteven@gmail.com>
2024-02-25 19:42:41 +0000

    Over half bucketed

--- audioScraper/deleter.py
+++ audioScraper/deleter.py
@@ -12,21 +12,18 @@ if __name__ == "__main__":
         with open('./bucketLog.txt', "a") as logFile:
             with open(sys.argv[1], "r") as datasetFile:
                 headerLine = datasetFile.readline()
-                successFile.write(headerLine[:-1] + ",bucket\n")
+                # successFile.write(headerLine[:-1] + ",bucket\n")
                 for line in datasetFile:
                     dataRow = line.strip().split(",")
                     title = ''.join(dataRow[0].split())
                     artist = ''.join(dataRow[1].split())
-
                     if os.path.isfile(sys.argv[2] + '/converted/' + title + '-' + artist + '.wav'):
                         successFile.write(line[:-1] + ',' + title + '-' + artist + '.wav' + '\n')
-                        """
                         args = ['rm', sys.argv[2] + '/converted/' + title + '-' + artist + '.wav']
                         output = subprocess.run(" ".join(args), stdout=logFile, stderr=logFile, shell=True)
                         if output.returncode != 0:
                             logFile.write("FAILURE RM: " + title + artist + "\n")
                         else:
                             logFile.write("SUCCESS: " + title + artist + "\n")
-                        """    
                     else:
                         logFile.write("FAILURE NOFILE: " + title + "-" + artist + "\n")
